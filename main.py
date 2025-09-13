c37ede129c6cf348febf72aa4895e425663492f0
robonilla <robonilla@gmail.com>
2025-09-13 21:41:43 +1000

    Creating Player class and drawing a triangle in the middle of the screene

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 import constants
+import player as ply
 
 def main():
     print("Starting Asteroids!")
@@ -12,11 +13,13 @@ def main():
     game_clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode(size=(constants.SCREEN_WIDTH,constants.SCREEN_HEIGHT))
+    player = ply.Player(constants.SCREEN_WIDTH/2,constants.SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
+        player.draw(screen)
         pygame.display.flip()
         dt = game_clock.tick(60)/1000
 
