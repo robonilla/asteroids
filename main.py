340a7111c0d8365faa39570cf2d8f309f344b82a
robonilla <robonilla@gmail.com>
2025-09-14 21:56:58 +1000

    Adding rotation to the player

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         dt = game_clock.tick(60)/1000
